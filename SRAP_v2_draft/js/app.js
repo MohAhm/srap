0d358d95ba3e48cc9980a69868a5f0ddb9b71b8d
Mohamed <mo7a93med@gmail.com>
2016-12-01 18:56:52 +0100

    Refactoring

--- SRAP_v2_draft/js/app.js
+++ SRAP_v2_draft/js/app.js
@@ -1,12 +1,22 @@
+
+var new_tbody = document.createElement('tbody');
+
+populate_with_new_rows(new_tbody);
+old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
+
+
+
 $(document).ready(function(){
-			$("#listItem01img").click(function(){
-				$("#listItem01").remove();
-			});
+	$("#listItem01img").click(function(){
+		$("#listItem01").remove();
+	});
 			
-			$("#listItem02img").click(function(){
-				$("#listItem02").remove();
-			});
-		});
+	$("#listItem02img").click(function(){
+		$("#listItem02").remove();
+	});
+});
+
+
 
 $("#from").datepicker({
 	dateFormat: "yy-mm-dd"
