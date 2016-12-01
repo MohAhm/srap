9cba56063de2206c15824d8776e1807dfc56ff34
Mohamed <mo7a93med@gmail.com>
2016-12-01 19:20:00 +0100

    datepicker error fixt

--- SRAP_v2_draft/js/app.js
+++ SRAP_v2_draft/js/app.js
@@ -1,11 +1,4 @@
 
-var new_tbody = document.createElement('tbody');
-
-populate_with_new_rows(new_tbody);
-old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
-
-
-
 $(document).ready(function(){
 	$("#listItem01img").click(function(){
 		$("#listItem01").remove();
