abdf54e243d0cac541ce501962d16db16ee7f4f1
Mohamed <mo7a93med@gmail.com>
2016-12-06 22:00:34 +0100

    Merge branch 'master' into mockup-v3
    # Conflicts:
    #	SRAP_v2/index.php
    #	SRAP_v2_draft/js/app.js

--- SRAP_v2/index.php
+++ SRAP_v2/index.php
@@ -1,13 +1,5 @@
-<?php
-    session_start();
-    include 'connect_mysql.php';
-    if ($_SESSION['username']=="" ) {
-        header("Location: login.php");
-        exit;
-    }
-?>
-
 <!DOCTYPE html>
+
 <html lang="en">
     <head>
         <title>Room Allocation Planner</title>
@@ -25,6 +17,14 @@
 
         <!-- Custom CSS -->
         <link rel="stylesheet" href="css/style.css">
+<?php
+		session_start();
+		include 'connect_mysql.php';
+		if ($_SESSION['username']=="" ) {
+		header("Location: login.php");
+		exit;
+ }
+		?>
     </head>
     <body>        
         <!-- topbar -->
@@ -34,8 +34,13 @@
 
                 <div id="navbar">
                     <div class="float-xs-right">
+<<<<<<< HEAD:SRAP_v2_draft/index.php
                         <p class="d-inline mr-3 hidden-sm-down"><span class="name"><?php echo $_SESSION["username"]; ?></span></p> 
                         <a href="logout.php" class="btn btn-outline-warning">Logout</a>
+=======
+                        <p class="d-inline mr-3 hidden-sm-down">Welcome: <span class="name"><?php echo $_SESSION["username"]; ?></span></p> 
+                        <button type="submit" class="btn btn-outline-warning"><a href="logout.php">Logout</a></button>
+>>>>>>> master:SRAP_v2/index.php
                     </div>
                 </div>
             </div>
@@ -68,16 +73,16 @@
                     
                     <div class="container-fluid">
                         <div class="row">
-                            <div class="col-md-4"> 
+                            <div class="col-md-4">
                                 <!-- form-->
                                 <form action="booking.php" method="post">
-                                    <fieldset class="form-group mb-2 has-warning">
+                                    <fieldset class="form-group mb-2">
                                         <label for="from">Date From:</label>
-                                        <input type="text" class="form-control form-control-warning" id="from" placeholder="yy-mm-dd" autocomplete="off"  name="from">
+                                        <input type="text" class="form-control" id="from" placeholder="yy-mm-dd" autocomplete="off"  name="from">
                                     </fieldset>
-                                    <fieldset class="form-group mb-2 has-warning">
+                                    <fieldset class="form-group mb-2">
                                         <label for="to">Date To:</label>
-                                        <input type="text" class="form-control form-control-warning" id="to" placeholder="yy-mm-dd" autocomplete="off"  name="to">
+                                        <input type="text" class="form-control" id="to" placeholder="yy-mm-dd" autocomplete="off"  name="to">
                                     </fieldset>
                                     <fieldset class="form-group mb-2">
                                         <label for="seats">Seats:</label>
@@ -112,6 +117,7 @@
                                 <h3 class="my-2">My Bookings:</h3>
 								<ul id="myBooking" class="list-group">
 								<?php
+<<<<<<< HEAD:SRAP_v2_draft/index.php
 								    $select_path = "SELECT * from reservation";
 									$result = mysqli_query($conn, $select_path);
 
@@ -127,6 +133,23 @@
 												    </h4>
 												    ' . $row['room_name'] . ', ' . $row['seats'] . ' Seat
 											    </li>';
+=======
+								$select_path = "SELECT *
+												from reservation";
+												$result = mysqli_query($conn, $select_path);
+											while($row = $result->fetch_assoc()){
+												echo '<li class="list-group-item">
+														<h4 class="list-group-item-heading">
+														' . $row['date_from'] . ' - ' . $row['date_to'] . '
+														<span class="tag tag-pill float-xs-right">
+														<a href="#">
+                                                    <img class="icon" id="listItem01img" src="img/cancel.svg" alt="icon">
+													</a> 
+													</span>
+													</h4>
+													' . $row['room_name'] . ', ' . $row['seats'] . ' Seat
+													</li>';
+>>>>>>> master:SRAP_v2/index.php
 											}
 								?>
                                 </ul>
---
+++ SRAP_v2/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+			$("#listItem01img").click(function(){
+				$("#listItem01").remove();
+			});
+			
+			$("#listItem02img").click(function(){
+				$("#listItem02").remove();
+			});
+		});
+
+$("#from").datepicker({
+	dateFormat: "yy-mm-dd"
+});
+
+$("#to").datepicker({
+	dateFormat: "yy-mm-dd"
+});
