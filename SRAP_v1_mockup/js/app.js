7b3f51f9aa19e09b4d9c7438b98fe322dccc0832
Mohamed <mo7a93med@gmail.com>
2016-12-06 21:45:49 +0100

    add and delete booking (locally) with jQuery

--- SRAP_v1_mockup/js/app.js
+++ SRAP_v1_mockup/js/app.js
@@ -1,17 +1,71 @@
-$(document).ready(function(){
-			$("#listItem01img").click(function(){
-				$("#listItem01").remove();
-			});
-			
-			$("#listItem02img").click(function(){
-				$("#listItem02").remove();
-			});
-		});
 
-$("#from").datepicker({
-	dateFormat: "yy-mm-dd"
+var $dateInputFrom = $("#from");
+var $dateInputTo   = $("#to");
+var $seatsSelect   = $("#seats");
+var $roomSelect    = $("#room");
+var $addButton     = $("#book");
+var $booking       = $("#myBooking");
+
+$dateInputFrom.datepicker({
+	dateFormat: "yy-mm-dd",
+	minDate: 0
 });
 
-$("#to").datepicker({
-	dateFormat: "yy-mm-dd"
+$dateInputTo.datepicker({
+	dateFormat: "yy-mm-dd",
+	minDate: 2
 });
+
+
+var createNewBookElement = function(dateFrom, dateTo, seats, room)
+{
+	var $listItem = $("<li></li>");
+	var $date = $("<h4></h4>");
+	var $info = $("<p></p>");
+
+	$listItem.addClass("list-group-item");
+
+	$date.text(dateFrom + " -- " + dateTo);
+	$date.addClass("list-group-item-heading");
+	$date.append(
+		$("<span class='tag tag-pill float-xs-right'/>").append(
+			$("<a href='#'/>").append(
+				$("<img class='icon' src='img/cancel.svg' alt='icon'/>")
+			)
+		)	
+	);
+
+	$info.text(room + ", " + seats + " Seat");
+	$info.addClass("mb-0");
+
+	$listItem.append($date);
+	$listItem.append($info);
+
+	return $listItem;
+};
+
+// add booking
+$addButton.click(function()
+{
+	console.log("Add booking ...");
+
+	// here: should be some validation on inputs before calling createNewBookElement function
+	var $listItem = createNewBookElement($dateInputFrom.val(), $dateInputTo.val(), 
+									     $seatsSelect.val(), $roomSelect.val());
+
+	$booking.append($listItem);
+
+	// reset values
+	$dateInputFrom.val("");
+	$dateInputTo.val("");
+	$seatsSelect.val(0);
+});
+
+// cancel booking
+$booking.on("click", "a", function() 
+{
+	console.log("Cancel");
+
+	$(this).parentsUntil(".list-group").remove();
+});
+
--- SRAP_v2_draft/js/app.js
+++ SRAP_v2_draft/js/app.js
@@ -1,20 +1,75 @@
 
-$(document).ready(function(){
-	$("#listItem01img").click(function(){
-		$("#listItem01").remove();
-	});
-			
-	$("#listItem02img").click(function(){
-		$("#listItem02").remove();
-	});
+var $dateInputFrom = $("#from");
+var $dateInputTo   = $("#to");
+var $seatsSelect   = $("#seats");
+var $roomSelect    = $("#room");
+var $addButton     = $("#book");
+var $booking       = $("#myBooking");
+
+$dateInputFrom.datepicker({
+	dateFormat: "yy-mm-dd",
+	minDate: 0
+});
+
+$dateInputTo.datepicker({
+	dateFormat: "yy-mm-dd", 
+	minDate: 2
 });
 
 
+var createNewBookElement = function(dateFrom, dateTo, seats, room)
+{
+	var $listItem = $("<li></li>");
+	var $date = $("<h4></h4>");
+	var $info = $("<p></p>");
 
-$("#from").datepicker({
-	dateFormat: "yy-mm-dd"
+	$listItem.addClass("list-group-item");
+
+	$date.text(dateFrom + " -- " + dateTo);
+	$date.addClass("list-group-item-heading");
+	$date.append(
+		$("<span class='tag tag-pill float-xs-right'/>").append(
+			$("<a href='#'/>").append(
+				$("<img class='icon' src='img/cancel.svg' alt='icon'/>")
+			)
+		)	
+	);
+
+	$info.text(room + ", " + seats + " Seat");
+	$info.addClass("mb-0");
+
+	$listItem.append($date);
+	$listItem.append($info);
+
+	return $listItem;
+};
+
+// add booking
+$addButton.click(function()
+{
+	console.log("Add booking ...");
+
+	// here: should be some validation on inputs before calling createNewBookElement function
+	var $listItem = createNewBookElement($dateInputFrom.val(), $dateInputTo.val(), 
+									     $seatsSelect.val(), $roomSelect.val());
+
+	$booking.append($listItem);
+
+	// ## add values to the db ##
+
+	// reset values
+	$dateInputFrom.val("");
+	$dateInputTo.val("");
+	$seatsSelect.val(0);
 });
 
-$("#to").datepicker({
-	dateFormat: "yy-mm-dd"
+// cancel booking
+$booking.on("click", "a", function() 
+{
+	console.log("Cancel");
+
+	$(this).parentsUntil(".list-group").remove();
+
+	// ## delete item from db ##
 });
+
